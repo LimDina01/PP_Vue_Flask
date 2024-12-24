ed4fa21a5f0cdc6970b527f9b4f7f49213c498f9
Neaklahan <154498982+Neaklahan@users.noreply.github.com>
2024-12-24 10:44:58 +0700

    Do Customer Front-End

--- app.py
+++ app.py
@@ -40,6 +40,13 @@ def product():
     return render_template('admin/product.html')
 
 
+# =========================================================
+
+@app.route('/customer/index')
+def customer():
+    return render_template('customer/index.html')
+
+
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()  # Create tables if they don't exist
